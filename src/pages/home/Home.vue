dab0b96cf8049f880dbe2647271bd4c3788c946e
congcong <862348941@qq.com>
2021-08-14 11:32:02 +0800

    周末出游板块完成

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,6 +4,7 @@
         <home-swiper></home-swiper>
         <home-Icons></home-Icons>
         <home-Recommend></home-Recommend>
+        <home-Weekend></home-Weekend>
     </div>
 </template>
 
@@ -12,13 +13,16 @@ import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
+
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
     HomeIcons,
-    HomeRecommend
+    HomeRecommend,
+    HomeWeekend
   }
 }
 </script>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of RecommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img :src="item.imgUrl" class="item-img" alt="" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      RecommendList: [{
+        id: '0001',
+        imgUrl: 'https://imgs.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+        title: '京城周末撒欢',
+        desc: '在帝都过周末，不仅仅是城中游！'
+      }, {
+        id: '0002',
+        imgUrl: 'https://imgs.qunarzz.com/sight/source/1811/7e/476589267ebb41.jpg_r_640x214_bf599709.jpg',
+        title: '京城溜娃必去',
+        desc: '德智体美劳全面发展的亲子日，这些地方该去看看...'
+      }, {
+        id: '0003',
+        imgUrl: 'https://imgs.qunarzz.com/sight/source/1505/aa/7baaf8a851d221.jpg_r_640x214_1431200f.jpg',
+        title: '帝都必打卡',
+        desc: '来北京必去的景点非这些地方莫属'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl';
+    .title
+        line-height: 0.8rem
+        background-color: #eee
+        text-indent: 0.2rem
+    .item-img-wrapper
+        overflow hidden
+        padding .1rem
+        height 0
+        padding-bottom 37.09%
+        .item-img
+            width 100%
+    .item-info
+        padding .1rem
+        .item-title
+            line-height .54rem
+            font-size .32rem
+            ellipsis()
+        .item-desc
+            line-height .4rem
+            color:#ccc
+            ellipsis()
+</style>
